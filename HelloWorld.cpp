f524f990d633666ab309cde76e2ee72d2efe2f50
wenfengzzx <zhangzhenxing91@163.com>
2016-05-25 21:32:54 +0800

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,11 @@
 int main()
 {
   // first edit in original one
-  std::cout<<"it is a hello world program\n";
-  // first edit, add some comments
+  bool tag=false;
+  if(tag){
+    std::cout<<"it is a new hello world program\n";
+    // first edit, add some comments
+  }
+  
   return 0;
 }
